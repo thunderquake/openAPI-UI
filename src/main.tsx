f9aeac3b1c63784274ac3b01c0f39e12486d7680
Anna Dmytriieva <loooverrr12@gmail.com>
2024-12-10 12:41:06 +0200

    feat: parse files

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import MainPage from "./pages/MainPage";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <MainPage />
+  </StrictMode>
+);
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,21 @@
+import FileInput from "@/components/FileInput";
+import { bundleFromString, createConfig } from "@redocly/openapi-core";
+import { useState } from "react";
+
 const MainPage = () => {
-  return (
-    <>
-      <div></div>;
-    </>
-  );
+  const [parcedObj, setParcedObj] = useState();
+
+  const parcetoObj = async (yamlString: string) => {
+    const config = await createConfig({});
+    const result = await bundleFromString({ config, source: yamlString });
+    return result.bundle.parsed;
+  };
+  const onChange = async (yamlString: string) => {
+    const objResponse = await parcetoObj(yamlString);
+    setParcedObj(objResponse);
+  };
+
+  return <FileInput onChange={onChange} />;
 };
 
 export default MainPage;
