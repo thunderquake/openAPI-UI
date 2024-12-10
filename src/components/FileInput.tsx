26cdb829e318c6319d134d6d83c07acd05783e84
Anna Dmytriieva <loooverrr12@gmail.com>
2024-12-10 12:41:06 +0200

    feat: parse files

--- src/components/FileInput.tsx
+++ src/components/FileInput.tsx
@@ -1,1 +1,41 @@
-const FileInput = () => {};
+import { ChangeEvent, useEffect, useState } from "react";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+
+const FileInput = ({ onChange }: { onChange: (string: string) => void }) => {
+  const [yamlString, setYamlString] = useState("");
+
+  const handleFileUpload = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const yamlContent = e.target?.result;
+        if (typeof yamlContent === "string") {
+          setYamlString(yamlContent);
+        }
+      };
+      reader.readAsText(file);
+    }
+  };
+
+  useEffect(() => {
+    onChange(yamlString);
+  }, [onChange, yamlString]);
+  console.log(yamlString);
+
+  return (
+    <div className="grid w-full max-w-sm items-center gap-2">
+      <Label htmlFor="yaml-file-input">Upload OpenAPI Definition (YAML)</Label>
+      <Input
+        id="yaml-file-input"
+        type="file"
+        accept=".yaml,.yml"
+        multiple={false}
+        onChange={handleFileUpload}
+      />
+    </div>
+  );
+};
+
+export default FileInput;
